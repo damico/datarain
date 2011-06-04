f2c51873a7158a7574b5cd0b05160f153513e695
birouska <birouska@ea5416b4-66e9-7760-bd4b-3667ba01ca10>
2011-06-04 18:45:07 +0000

    package with utils tools

---
+++ datarain/src/com/google/code/datarain/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.google.code.datarain.utils;
+
+public interface Constants {
+	
+	public static final String IMAGE_SOURCE = "C:/Users/Birouska/Pictures/";
+	public static final String IMAGE_INTERMEDIATE_TARGET = "C:/Users/Birouska/Pictures";
+	public static final String IMAGE_FINAL_TARGET = "C:/Users/Birouska/Pictures";
+	
+
+}
